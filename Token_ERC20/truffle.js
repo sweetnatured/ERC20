db2a29320c80dbf8f6081de8fd7db5e38b0adb62
sweetnatured <beskardessalih@gmail.com>
2021-11-22 21:44:41 +0100

    S

---
+++ Token_ERC20/truffle.js
@@ -0,0 +1,44 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "<Infura PROJECT ID>";
+//
+// const fs = require('fs');
+const mnemonic = "<METAMASK SEED>";
+
+module.exports = {
+
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(phone tuition snap food orchard dirt pool struggle winter angle forest upgrade, `https://rinkeby.infura.io/v3/c4074cd8d93c4901a5a7d067b8ae65e1`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+}
